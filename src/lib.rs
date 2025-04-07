e66f77223d56ceed259981de5f87290f8622d7f0
Graham Kelly <gkgoat6700@gmail.com>
2025-04-07 16:12:01 +0000

    llm

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+use core::ops::Deref;
+pub trait LLMMut{
+    fn start(&mut self, system: &str) -> impl LLMInstance;
+}
+pub trait LLMRef{
+    fn start(&self, system: &str) -> impl LLMInstance;
+}
+pub trait LLMInstance{
+    fn send(&mut self, user: impl Iterator<Item: Deref<Target = str>>) -> impl embedded_io::Read;
+}
+pub trait AsyncLLMMut{
+    async fn start(&mut self, system: &str) -> impl AsyncLLMInstance;
+}
+pub trait AsyncLLMRef{
+    async fn start(&self, system: &str) -> impl AsyncLLMInstance;
+}
+pub trait AsyncLLMInstance{
+    async fn send(&mut self, user: impl Iterator<Item: Deref<Target = str>>) -> impl embedded_io_async::Read;
+}
