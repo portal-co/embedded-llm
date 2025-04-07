9dda468fa5d9313c6ef97345a639451c8822c1d9
Graham Kelly <gkgoat6700@gmail.com>
2025-04-07 16:19:14 +0000

    error

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,36 @@
 
 use core::ops::Deref;
 
+use embedded_io::ErrorType;
 use futures_core::Stream;
-pub trait LLMMut{
-    fn start(&mut self, system: &str) -> impl LLMInstance;
+pub trait LLMMut: ErrorType {
+    fn start(&mut self, system: &str)
+    -> Result<impl LLMInstance<Error = Self::Error>, Self::Error>;
 }
-pub trait LLMRef{
-    fn start(&self, system: &str) -> impl LLMInstance;
+pub trait LLMRef: ErrorType {
+    fn start(&self, system: &str) -> Result<impl LLMInstance<Error = Self::Error>, Self::Error>;
 }
-pub trait LLMInstance{
-    fn send(&mut self, user: impl Iterator<Item: Deref<Target = str>>) -> impl embedded_io::Read;
+pub trait LLMInstance: ErrorType {
+    fn send(
+        &mut self,
+        user: impl Iterator<Item: Deref<Target = str>>,
+    ) -> Result<impl embedded_io::Read<Error = Self::Error>, Self::Error>;
 }
-pub trait AsyncLLMMut{
-    async fn start(&mut self, system: &str) -> impl AsyncLLMInstance;
+pub trait AsyncLLMMut: ErrorType {
+    async fn start(
+        &mut self,
+        system: &str,
+    ) -> Result<impl AsyncLLMInstance<Error = Self::Error>, Self::Error>;
 }
-pub trait AsyncLLMRef{
-    async fn start(&self, system: &str) -> impl AsyncLLMInstance;
+pub trait AsyncLLMRef: ErrorType {
+    async fn start(
+        &self,
+        system: &str,
+    ) -> Result<impl AsyncLLMInstance<Error = Self::Error>, Self::Error>;
 }
-pub trait AsyncLLMInstance{
-    async fn send(&mut self, user: impl Stream<Item: Deref<Target = str>>) -> impl embedded_io_async::Read;
+pub trait AsyncLLMInstance: ErrorType {
+    async fn send(
+        &mut self,
+        user: impl Stream<Item: Deref<Target = str>>,
+    ) -> Result<impl embedded_io_async::Read<Error = Self::Error>, Self::Error>;
 }
